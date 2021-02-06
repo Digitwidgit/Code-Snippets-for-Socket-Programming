56d65a8195d001f21aca30298c12a2ae2e1a5459
Digit_Widgit <78665696+Digitwidgit@users.noreply.github.com>
2021-02-06 15:42:44 -0700

    Update xipaddress.py

--- xipaddress.py
+++ xipaddress.py
@@ -1,6 +1,8 @@
 
 from requests import get
 
-
+#Get your external ip address and print it to the screen.
+#This can be useful, since most of the code I have been seeing is -
+#getting the internal ip address.
 ip = get('https://api.ipify.org').text
 print('My public IP address is: {}'.format(ip))
